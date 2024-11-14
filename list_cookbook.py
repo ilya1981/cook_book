d3492beaac93e27c4a39e2feed7e68d4adc024af
Ilya81 <ilya110881@gmail.com>
2024-11-14 21:06:26 +0300

    add cook_book

--- list_cookbook.py
+++ list_cookbook.py
@@ -1,16 +1,45 @@
-import json
+# Задание №1
+def dickt_form(dict_):
+    for item_a in dict_:
+        print (item_a,':')
+        for item_b in dict_[item_a]:
+            print (item_b)
+
 with open('Receipt.txt', encoding='utf-8') as file:
     cook_book = {}
     for item in file:
-        receipt = item.strip()
+        receipt = item.strip() 
         count = file.readline()
-        ingredients = []
-        for p in range(int(count)):
+        products = []
+        for prod in range(int(count)):
             rec = file.readline().strip().split('|')
             product, quantity, name = rec
-            ingredients.append({'ingridient_name': product, 'quantity': quantity, 'measure': name })
-            cook_book[receipt] = ingredients
+            products.append({'ingredient_name': product, 'quantity': int(quantity), 'measure': name})
+            cook_book[receipt] = products
         file.readline()
-    print(cook_book)
+    dickt_form(cook_book)
 
+#Задание №2
+def get_shop_list_by_dishes(dishes, person_count):
+    in_total = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for count in cook_book[dish]:
+                if count['ingredient_name'] in in_total:
+                    in_total[count['ingredient_name']]['quantity'] += count['quantity'] * person_count
+                else:
+                    in_total[count['ingredient_name']] = {'measure': count['measure'],'quantity': (int(quantity) * person_count)}
+        else:
+            print('Такого блюда нет в книге')
+            print(in_total)
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+
+#Задание №3
+
+
+
+
+
+ 
     
